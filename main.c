3808facccfa67ee1fa1852a3518f422c5ac6b9ae
Andrei <yell4510@gmail.com>
2021-03-15 00:24:15 +0300

    some changes

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
 
-    printf("Enter your first name: ");
+    printf("Enter YOUR first name: ");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
